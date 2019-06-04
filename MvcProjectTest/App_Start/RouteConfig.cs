036324050064cd3382daa60bcf6f0c297e09900c
Abbie Ke <abbieke222@gmail.com>
2019-06-04 18:53:31 +0800

    搜尋錯誤修改

--- MvcProjectTest/App_Start/RouteConfig.cs
+++ MvcProjectTest/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace MvcProjectTest
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Search",
+                url: "Book/{name}",
+                defaults: new { controller = "Book", action = "Index", name = UrlParameter.Optional }
+            );
         }
     }
 }
