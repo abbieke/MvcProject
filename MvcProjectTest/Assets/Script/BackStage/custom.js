5ea964b5ee08a1c56561e037777614cb4f9ed0b6
Abbie Ke <abbieke222@gmail.com>
2019-06-14 04:36:28 +0800

    123

--- MvcProjectTest/Assets/Script/BackStage/custom.js
+++ MvcProjectTest/Assets/Script/BackStage/custom.js
@@ -716,40 +716,7 @@ if (typeof NProgress != 'undefined') {
 		if ($('.canvasDoughnut').length){
 			//帶首頁中的變數，首頁做Json
             var chart_doughnut_settings = indexUnderCircleJson;
-   //         {
-			//	type: 'doughnut',
-			//	tooltipFillColor: "rgba(51, 51, 51, 0.55)",
-			//	data: {
-			//		labels: [
-			//			"Symbian",
-			//			"Blackberry",
-			//			"Other",
-			//			"Android",
-			//			"IOS"
-			//		],
-			//		datasets: [{
-			//			data: [15, 20, 30, 10, 30],
-			//			backgroundColor: [
-			//				"#BDC3C7",
-			//				"#9B59B6",
-			//				"#E74C3C",
-			//				"#26B99A",
-			//				"#3498DB"
-			//			],
-			//			hoverBackgroundColor: [
-			//				"#CFD4D8",
-			//				"#B370CF",
-			//				"#E95E4F",
-			//				"#36CAAB",
-			//				"#49A9EA"
-			//			]
-			//		}]
-			//	},
-			//	options: { 
-			//		legend: false, 
-			//		responsive: false 
-			//	}
-			//}
+  
 		
 			$('.canvasDoughnut').each(function(){
 				
